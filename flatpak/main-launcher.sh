b7e2ac2309d1dde9dc98ef5687acd0c3a7f5b939
Devid Dondorf <96085754+deviddondorf@users.noreply.github.com>
2025-11-28 22:48:35 +0100

    sd

--- flatpak/main-launcher.sh
+++ flatpak/main-launcher.sh
@@ -1,3 +1,18 @@
-#!/bin/bash
-exec /usr/bin/python3 /app/share/abv-calculator/main.py
+#!/bin/sh
+# main-launcher.sh - stabil für Flatpak + PySide6
+
+# Python-Modulpfad
+export PYTHONPATH=/app/lib/python3.11/site-packages
+
+# Qt-Plugins
+export QT_PLUGIN_PATH=/app/lib/python3.11/site-packages/PySide6/Qt/plugins
+
+# Qt Plattform-Fallback
+export QT_QPA_PLATFORM=wayland;xcb
+
+# Python-Home setzen (optional, kann Probleme verhindern)
+export PYTHONHOME=/app
+
+# Starten der App
+exec python3 /app/share/abv-calculator/main.py
 
